51420b2a3cab9bd64bfdb530f6f1decee0e775ce
80686 <manuel.schneider@wikimedia.ch>
2014-10-11 22:36:19 +0200

    regular file upload works

---
+++ examples/upload/upload_file_by_url.php
@@ -0,0 +1,21 @@
+<?php
+  include( '../../phpapilib.php' );
+  
+  if( !isset( $argv[1] ) ) {
+    echo 'please specify URL'."\n";
+    exit( 1 );
+  }
+
+  # check if 1st argument is a file
+  #if( !file_exists( $argv[1] ) ) {
+  #  echo 'URL is not readable'."\n";
+  #  exit( 127 );
+  #}
+  
+  # login
+  login();
+  $filename = basename( $argv[1] );
+  echo $filename;
+  # upload file
+  upload_file_url( $argv[1], $filename, 'upload-text' );
+?>
