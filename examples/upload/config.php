4a9951e710bdd29b28da95514925f52fe330bb1d
80686 <manuel.schneider@wikimedia.ch>
2014-10-12 17:28:58 +0200

    add example config file

---
+++ examples/upload/config.php
@@ -0,0 +1,7 @@
+<?php
+  define( 'URL', 'http://commons.wikimedia.org/w/api.php' );
+  define( 'USERNAME', 'Username' );
+  define( 'PASSWORD', 'secret password' );
+#  define( 'DEBUG', true );
+#  define( 'CHUNKSIZE', 5 * 1024 * 1024 );  # in Bytes - default is 20 MB
+?>
