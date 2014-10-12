5b87de74f1d1d893392f93576d9e670019cb7154
80686 <manuel@mirabilis.80686-net.de>
2014-10-12 19:49:13 +0200

    improved upload script, added text parameter

--- examples/upload/upload_file.php
+++ examples/upload/upload_file.php
@@ -1,21 +1,37 @@
 <?php
   include( '../../phpapilib.php' );
+  $error = '';
   
-  if( !isset( $argv[1] ) ) {
-    echo 'please specify local file'."\n";
-    exit( 1 );
-  }
-
-  # check if 1st argument is a file
-  if( !file_exists( $argv[1] ) ) {
-    echo 'file is not readable'."\n";
-    exit( 127 );
+  # check parameters
+  if( $argc == 3 ) {
+    # read parameters
+    $filepath = $argv[1];
+    $text = $argv[2];
+    
+    # check local file
+    if( !is_readable( $filepath ) ) $error .= 'file "'.$filepath.'" is not readable'."\n";
+    # check text - if it is a file read contents
+    if( is_readable( $text ) ) $text = file_get_contents( $text );
+  } else {
+    # parameter count doesn't match
+    $error .= 'script expects two paramters'."\n";
   }
   
-  # login
-  login();
-  $filename = basename( $argv[1] );
-  echo $filename;
-  # upload file - 2nd parameter could be $filename, when NULL the basename will be taken
-  upload_file( $argv[1], NULL, 'upload-text' );
+  if( $error == '' ) {
+    # login
+    login();
+    $filename = basename( $argv[1] );
+    # upload file - 2nd parameter could be $filename, when NULL the basename will be taken
+    upload_file( $filename, NULL, $text );
+  } else {
+    # deal with errors
+    
+    echo $error;
+    echo "\n";
+    echo 'usage: '.$argv[0].' filename text'."\n";
+    echo '* filename: local file you want to upload'."\n";
+    echo '* text: text to be put on the file description page.'."\n";
+    echo '** either: the text itself (string - do not forget to "quote" or escape)'."\n";
+    echo '** or    : the filename where the text can be read from'."\n";
+  }
 ?>
